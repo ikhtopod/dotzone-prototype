6cad6b12e727141758509d5656d3ec6fd1bfebbd
reclue <vitaly@lifanoff.ru>
2020-11-08 20:26:25 +0300

    updated TouchManager

--- Gameplay/Camera/MainCameraTarget.gd
+++ Gameplay/Camera/MainCameraTarget.gd
@@ -7,4 +7,5 @@ signal moved
 func _process(delta):
 	for touch in TouchManager.current_touch.GetOnlyPressed():
 		position += (touch.GetPosition() - (get_viewport_rect().size / 2.0)) * delta
+		print(touch.GetCurrentType())
 		emit_signal("moved")
--- Library/Singleton/TouchManager.gd
+++ Library/Singleton/TouchManager.gd
@@ -1,33 +1,65 @@
 extends Node
 
 
-class TouchEventStat:
-	var m_index: int setget SetIndex, GetIndex
-	var m_pressed: bool setget SetPressed, IsPressed
-	var m_position: Vector2 setget SetPosition, GetPosition
+enum EInputEventScreenType { NONE = 0, TOUCH, DRAG }
+
+
+class TouchEventStatParameters:
+	var m_currentType = EInputEventScreenType.NONE
 	
-	func _init(index: int = 0, pressed: bool = false, position := Vector2()) -> void:
+	var m_index: int = 0
+	var m_pressed: bool = false
+	var m_position: Vector2 = Vector2()
+	
+	func SetCurrentType(currentType) -> TouchEventStatParameters:
+		m_currentType = currentType
+		return self
+	
+	func SetIndex(index: int) -> TouchEventStatParameters:
 		m_index = index
+		return self
+	
+	func SetPressed(pressed: bool) -> TouchEventStatParameters:
 		m_pressed = pressed
+		return self
+	
+	func SetPosition(position: Vector2) -> TouchEventStatParameters:
 		m_position = position
+		return self
+
+
+class TouchEventStat:
+	var m_parameters: TouchEventStatParameters = null
+	
+	func _init(parameters: TouchEventStatParameters) -> void:
+		m_parameters = parameters
+	
+	func GetCurrentType():
+		return m_parameters.m_currentType
+	
+	func SetCurrentType(newType) -> void:
+		m_parameters.SetCurrentType(newType)
+		
+	func ResetCurrentType() -> void:
+		m_parameters.SetCurrentType(EInputEventScreenType.NONE)
 	
 	func GetIndex() -> int:
-		return m_index
+		return m_parameters.m_index
 	
 	func SetIndex(index: int) -> void:
-		m_index = index
+		m_parameters.SetIndex(index)
 	
 	func IsPressed() -> bool:
-		return m_pressed
+		return m_parameters.m_pressed
 	
 	func SetPressed(pressed: bool) -> void:
-		m_pressed = pressed
+		m_parameters.SetPressed(pressed)
 	
 	func GetPosition() -> Vector2:
-		return m_position
+		return m_parameters.m_position
 	
 	func SetPosition(position: Vector2) -> void:
-		m_position = position
+		m_parameters.SetPosition(position)
 
 
 # Класс, который хранит прикосновения к экрану,
@@ -39,7 +71,9 @@ class MultiTouch:
 	
 	func _init() -> void:
 		for index in range(MAX_TOUCH):
-			m_touch.push_back(TouchEventStat.new(index))
+			var tmpParams: TouchEventStatParameters = \
+				TouchEventStatParameters.new().SetIndex(index)
+			m_touch.push_back(TouchEventStat.new(tmpParams))
 	
 	# Получить размер внутреннего массива MultiTouch
 	func Size() -> int:
@@ -74,11 +108,15 @@ onready var current_touch := MultiTouch.new()
 func _input(event):
 	if event is InputEventScreenTouch:
 		if (event.index < current_touch.Size()):
+			current_touch.At(event.index).SetCurrentType(EInputEventScreenType.TOUCH)
+			
 			if (event.is_pressed()):
 				current_touch.At(event.index).SetPressed(true)
 				current_touch.At(event.index).SetPosition(event.position)
 			else:
 				current_touch.At(event.index).SetPressed(false)
+				current_touch.At(event.index).SetPosition(event.position)
 	elif event is InputEventScreenDrag:
 		if (event.index < current_touch.Size()):
+			current_touch.At(event.index).SetCurrentType(EInputEventScreenType.DRAG)
 			current_touch.At(event.index).SetPosition(event.position)
