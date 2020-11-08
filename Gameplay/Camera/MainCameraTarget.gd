6e8e1dd797561a9f09352c73cbdde4b88e7af1a2
reclue <vitaly@lifanoff.ru>
2020-11-09 00:15:59 +0300

    inverted movement and changed camera settings

--- Gameplay/Camera/MainCameraTarget.gd
+++ Gameplay/Camera/MainCameraTarget.gd
@@ -6,6 +6,5 @@ signal moved
 
 func _process(delta):
 	for touch in TouchManager.current_touch.GetOnlyDRAG():
-		position += touch.Relative().Get() * touch.Speed().Get().abs() * pow(delta, 1.8)
-		#touch.ResetParameters()
+		position -= touch.Relative().Get() * touch.Speed().Get().abs() * pow(delta, 1.5)
 		emit_signal("moved")
