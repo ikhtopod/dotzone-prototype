8e449e5fd3aabd2ddd1ea81aa205b86c6ed43946
reclue <vitaly@lifanoff.ru>
2020-11-11 16:24:49 +0300

    added dot sprites

--- Gameplay/Camera/MainCameraTarget.gd
+++ Gameplay/Camera/MainCameraTarget.gd
@@ -28,5 +28,7 @@ func _on_MainCameraTarget_drag(index: int, touch: TouchManager.TouchEventStat) -
 	if !touch:
 		return
 	
+	print_debug("Index touch: " + str(index))
+	
 	RelativePositionOffset(touch)
 	emit_signal("moved")
