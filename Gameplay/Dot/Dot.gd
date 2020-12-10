f85505eb3988511c620a63331fe8e648df3f291e
reclue <vitaly@lifanoff.ru>
2020-12-10 19:12:02 +0300

    updated selection and owner systems

--- Gameplay/Dot/Dot.gd
+++ Gameplay/Dot/Dot.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 
 const DefaultTextureResource = preload("res://Sprite/Dot/default.png")
-const SelectedTextureResource = preload("res://Sprite/Dot/selected.png")
-const LinkedTextureResource = preload("res://Sprite/Dot/linked.png")
 const LinkedSelectedTextureResource = preload("res://Sprite/Dot/linked_selected.png")
+const SelectionBorderTextureResource = preload("res://Sprite/Dot/selection_border.png")
+
 
 var m_point: Point = Point.new()
 var m_noise: float = 0.0 setget SetNoise, GetNoise
@@ -43,14 +43,47 @@ func SetColorByNoise() -> void:
 	$Sprite.self_modulate = m_color
 
 
+func HasOwner() -> bool:
+	return m_owner != null
+
+
+func CreateSpriteSelection() -> void:
+	if has_node("SpriteSelection"):
+		return
+	
+	var spriteSelection: Sprite = Sprite.new()
+	spriteSelection.name = "SpriteSelection"
+	spriteSelection.texture = SelectionBorderTextureResource
+	add_child(spriteSelection)
+
+func RemoveSpriteSelection() -> void:
+	if has_node("SpriteSelection"):
+		get_node("SpriteSelection").queue_free()
+
+
+func FingerTouchReaction() -> void:
+	if HasOwner():
+		if m_owner is OwnerPlayer:
+			pass
+		else:
+			# pari
+			m_owner = OwnerPlayer.new()
+			$Sprite.texture = LinkedSelectedTextureResource
+	else:
+		m_owner = OwnerPlayer.new()
+		$Sprite.texture = LinkedSelectedTextureResource
+	
+	GameManager.SetSelectedObject(self)
+
+
 func _on_Area2D_area_entered(area: Area2D):
 	if area.is_in_group(GameManager.FINGER_TOUCH_TAG):
-		GameManager.SetSelectedObject(self)
+		FingerTouchReaction()
 
-# Функции, реализуемые объектом, который можно выделять #
+# Функции, реализуемые объектом, который можно выделять
 func Select() -> void:
-	$Sprite.texture = SelectedTextureResource
+	CreateSpriteSelection()
 
 func Deselect() -> void:
-	$Sprite.texture = DefaultTextureResource
+	RemoveSpriteSelection()
 #########################################################
